1799aa9d78b71a45b33643bcf206c41f7f64cc70
Raphael Teixeira <raphaelvictor27@gmail.com>
2023-12-09 18:15:59 -0300

    fix: tag phone

--- users/entities/user.go
+++ users/entities/user.go
@@ -6,7 +6,7 @@ type User struct {
 	ID        int64     `db:"id"`
 	Name      string    `db:"name"`
 	Email     string    `db:"email"`
-	Phone     string    `db:"phone"`
+	Phone     string    `db:"phone_number"`
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
 }
